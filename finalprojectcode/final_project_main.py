a6fe5814d28653ead8bdc7f2ea1f61aa3524995b
lgroves29 <49950549+lgroves29@users.noreply.github.com>
2019-04-28 14:40:08 -0400

    Add files via upload

---
+++ finalprojectcode/final_project_main.py
@@ -0,0 +1,13 @@
+from game_level_one import *
+from throne_room import *
+from graphics import *
+
+
+
+def main():
+    win = GraphWin("Console",1000, 800)
+   # levelOne(win)
+    throneRoom(win)
+    
+   
+main()
