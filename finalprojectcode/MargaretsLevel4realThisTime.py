59b495432ccd8b4b9e65359d38ecebabf1ae6058
MargaretBassney <47510868+MargaretBassney@users.noreply.github.com>
2019-04-28 22:09:36 -0400

    Update MargaretsLevel4realThisTime.py

--- finalprojectcode/MargaretsLevel4realThisTime.py
+++ finalprojectcode/MargaretsLevel4realThisTime.py
@@ -48,14 +48,33 @@ def cow_level():
     spot3()
 
 
+class Shard_1:
+    def __init__(self, x, y):
+        self.shard_point1 = Point(x, y)
+        self.shard_x = x
+        self.shard_y = y
+        self.shard_point2 = Point(x + 20, y - 5)
+        self.shard_point3 = Point(x + 30, y +5)
+        self.shard_point4 = Point(x + 5, y + 10)
+        self.shard = Polygon(self.shard_point1, self.shard_point2, self.shard_point3, self.shard_point4)
+    def draw(self, win):
+        shard = Polygon(self.shard_point1, self.shard_point2, self.shard_point3, self.shard_point4)
+        self.shard.draw(win)
+        self.shard.setFill("Gray")
+    def undraw(self):
+        self.shard.undraw()
+        
 def my_level():
+    
     intro()
     win.getKey()
     cow_level()
     win.getKey()
-
     outro()
+    sword = Shard_1(100,200)
+    sword.draw(win)
 my_level()
+    
     
 
 
